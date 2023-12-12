355699ee214e8e651e8de6deaf3ee5528e297ebd
Fr3nk21 <104112709+Fr3nk21@users.noreply.github.com>
2023-12-13 10:26:40 +1100

    Day 01 exercises

--- 01_Day_Introduction/exercises.js
+++ 01_Day_Introduction/exercises.js
@@ -1,19 +1,55 @@
 // 1. Write a single line comment which says, comments can make code readable
+// comments can make code readable
 
 // 2. Write another single comment which says, Welcome to 30DaysOfJavaScript
+// Welcome to 30DaysOfJavaScript
 
 // 3. Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/*
+  comments can make code readable, easy to reuse and informative
+*/
 
 // 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+// That's done here
+let stringVar = 'Fr3nk'
+let booleanVar = true
+let undefinedVar = undefined
+let nullVar = null
 
 // 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+// That's done here
+console.log(typeof firstName)
+console.log(typeof lastName)
+console.log(typeof country)
+console.log(typeof city)
+console.log(typeof age)
+console.log(typeof isMarried)
 
 // 6. Declare four variables without assigning values
+let var01
+let var02
+let var03
+let var04
 
 // 7. Declare four variables with assigned values
+let var05 = "Fr3nk"
+let var06 = 56
+let var07 = false
+let var08 = null
 
 // 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = 'Fr3nk'
+let lastName = 'Bugu'
+let country = 'Australia'
+let city = 'Melbourne'
+let age = 34
+let isMarried = false
 
 // 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+let firstName = 'Fr3nk', lastName = 'Bugu', country = 'Australia', city = 'Melbourne', age = 34, isMarried = false
 
 // 10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 40
+let yourAge = 30
+console.log(myAge)
+console.log(yourAge)
