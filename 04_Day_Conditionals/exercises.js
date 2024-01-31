7471bfaf27200038e65e479f85b2689040b6a8d4
Fr3nk21 <104112709+Fr3nk21@users.noreply.github.com>
2024-01-31 16:09:56 +1100

    Last Exercise

--- 04_Day_Conditionals/exercises.js
+++ 04_Day_Conditionals/exercises.js
@@ -134,8 +134,12 @@ switch (season) {
   What is the day today? FrIDAy
   Friday is a working day.
 */
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 let userDay = prompt('Today is?');
-let day = userDay.toLowerCase();
+let day = capitalizeFirstLetter(userDay.toLowerCase());
 
 switch (day) {
   case 'monday':
@@ -167,5 +171,49 @@ switch (day) {
   Enter a month: FEbruary
   February has 28 days.
 */
+
+let userMonth = prompt(`What you choose?`);
+let month = userMonth.toLowerCase();
+
+switch(month) {
+  case 'january':
+    console.log('January has 31 days');
+    break;
+  case 'february':
+    console.log('February has 28 days');
+    break;
+  case 'march':
+    console.log('March has 31 days');
+    break;
+  case 'april':
+    console.log('April has 30 days');
+    break;
+  case 'may':
+    console.log('May has 31 days');
+    break;
+  case 'june':
+    console.log('June has 30 days');
+    break;
+  case 'july':
+    console.log('July has 31 days');
+    break;
+  case 'august':
+    console.log('August has 31 days');
+    break;
+  case 'september':
+    console.log('September has 30 days');
+    break;
+  case 'october':
+    console.log('October has 31 days');
+    break;
+  case 'november':
+    console.log('November has 30 days');
+    break;
+  case 'december':
+    console.log('December has 31 days');
+    break;
+  default:
+    console.log('Wrong month!')
+}
 
 // 2. Write a program which tells the number of days in a month, now consider leap year.
